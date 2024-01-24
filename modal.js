c51bfa151ad7f16bfe78535a15b27a88019e05b5
mohitmandal40 <mohitmandal40@gmail.com>
2024-01-24 10:16:04 +0400

    init

---
+++ modal.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Create modal with content and close button
+  var modal = document.createElement("div");
+  modal.id = "myModal";
+  modal.className = "modal";
+  modal.innerHTML = `
+      <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Some text in the Modal..</p>
+      </div>`;
+
+  document.getElementById("app").appendChild(modal);
+
+  modal.style.display = "block";
+
+  modal
+    .querySelector(".close")
+    .addEventListener("click", () => (modal.style.display = "none"));
+  window.addEventListener("click", (event) => {
+    if (event.target == modal) modal.style.display = "none";
+  });
+});
