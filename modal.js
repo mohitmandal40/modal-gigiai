36ca006c8c3efdc7d8dc66263f0dc7f358aebd67
mohitmandal40 <mohitmandal40@gmail.com>
2024-01-24 12:22:48 +0400

    modal changes

--- modal.js
+++ modal.js
@@ -1,23 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Create modal with content and close button
-  var modal = document.createElement("div");
-  modal.id = "myModal";
-  modal.className = "modal";
-  modal.innerHTML = `
-      <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Some text in the Modal..</p>
-      </div>`;
-
-  document.getElementById("app").appendChild(modal);
-
-  modal.style.display = "block";
-  //   test
-
-  modal
-    .querySelector(".close")
-    .addEventListener("click", () => (modal.style.display = "none"));
-  window.addEventListener("click", (event) => {
-    if (event.target == modal) modal.style.display = "none";
+var modal = document.createElement("div");
+(modal.id = "myModal"),
+  (modal.className = "modal-list"),
+  (modal.innerHTML = `
+    <div class="modal-content">
+      <span class="close">&times;</span>
+      <p>Some text in the Modal..</p>
+    </div>`),
+  document.getElementById("app").appendChild(modal),
+  localStorage.getItem("showPopup") ||
+    localStorage.setItem("showPopup", "true"),
+  "true" === localStorage.getItem("showPopup") &&
+    modal.classList.add("modal-visible"),
+  modal.querySelector(".close").addEventListener("click", () => {
+    modal.classList.remove("modal-visible"),
+      localStorage.setItem("showPopup", "false");
+  }),
+  window.addEventListener("click", (e) => {
+    e.target == modal &&
+      (modal.classList.remove("modal-visible"),
+      localStorage.setItem("showPopup", "false"));
   });
-});
